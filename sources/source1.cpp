410ff4b5e4c25647f00e0d7aa5ea8a48124d9491
cheng306 <johnnycheng0306@gmail.com>
2022-11-14 22:41:25 -0800

    local change

--- sources/source1.cpp
+++ sources/source1.cpp
@@ -8,42 +8,35 @@
 
 
 
-std::vector<int> veccc{1,2,3};
-
-void func1(int a){
-  std::cout << "here" <<std::endl;
-}
 
 
-template <typename T, typename U>
-bool max(T t, U u){
-  return t>u;
-}
-
-
-struct IntPair
+enum class Animals
 {
-public:
-	int m_first{};
-	int m_second{};
+    chicken, // 0
+    dog, // 1
+    cat, // 2
+    elephant, // 3
+    duck, // 4
+    snake, // 5
 
-	void set(int first, int second)
-	{
-		m_first = first;
-		m_second = second;
-	}
-	void print()
-	{
-		std::cout << "Pair(" << m_first << ", " << m_second << ")\n";
-	}
+    maxAnimals,
 };
 
-int adder(int, int);
+// Overload the unary + operator to convert Animals to the underlying type
+// adapted from https://stackoverflow.com/a/42198760, thanks to Pixelchemist for the idea
+constexpr double operator+(Animals a) noexcept
+{
+    return static_cast<std::underlying_type_t<Animals>>(a);
+}
+
+
 
 int main(){
 
 
-  std::cout << adder(3,5) <<std::endl;
+  std::cout << +Animals::snake << '\n'; 
+  double aa = 1.0;
+  std::cout << aa << std::endl;
 
 
 
